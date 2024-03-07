503a7318e01e8620df1c552f7a06c672c4248941
pfavero <paolo.faver@wheregroup.com>
2024-03-07 07:54:00 +0100

    handle connection

---
+++ src/utils/db.ts
@@ -0,0 +1,11 @@
+import { Pool } from 'pg';
+
+const conn: Pool = new Pool({
+  user: process.env.PGSQL_USER,
+  password: process.env.PGSQL_PASSWORD,
+  host: process.env.PGSQL_HOST,
+  port: Number(process.env.PGSQL_PORT),
+  database: process.env.PGSQL_DATABASE,
+});
+
+export default conn;
